37a7669d5afe56af70628f0f2f46452ca4072055
Bruno Pontees <silvapontes_bruno10@outlook.com>
2021-05-24 22:07:26 -0300

    Inserindo rotas para calculos 2

--- core/views.py
+++ core/views.py
@@ -4,4 +4,20 @@ from django.shortcuts import render, HttpResponse
 
 
 def hello(request, nome, idade):
-    return HttpResponse(f'<h1>Hello {nome.title()}, {idade} anos</h1>')
+    return HttpResponse(f'<h1>Hello Word, {nome.title()}, {idade} anos</h1>')
+
+
+def soma(request, num1, num2):
+    return HttpResponse(num1+num2)
+
+
+def subtracao(request, num1, num2):
+    return HttpResponse(num1 - num2)
+
+
+def multiplicacao(request, num1, num2):
+    return HttpResponse(num1 * num2)
+
+
+def divisao(request, num1, num2):
+    return HttpResponse(num1 / num2)
